db1345cadd4cfffc73947bb2ed275c394bfe8886
RohanSengupta326 <senguptarohan34@gmail.com>
2022-06-29 11:32:46 +0530

    api object created

--- lib/api/utcms.dart
+++ lib/api/utcms.dart
@@ -4,45 +4,47 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:get/get.dart';
 
-class UtcMs {
+class PostRequestInUtcMillisecond {
   Map<String, dynamic>? data;
 
   Future<void> fetchData(DateTimeRange date, TimeRange time) async {
-    String startDateTimeUtc = DateTime(date.start.day).toIso8601String();
-    
+    String startDateTimeUtc = DateTime(date.start.day).toUtc().toString();
+    String startDateTimeMilliseconds =
+        DateTime(date.start.day).millisecond.toString();
 
-    var startUtc = date.start.toUtc();
-    var endUtc = date.end.toUtc();
-    var startMs = time.startTime.minute * 60000;
-    var endMs = time.endTime.minute * 60000;
+    String endDateTimeUtc = DateTime(date.end.day).toUtc().toString();
+    String endDateTimeMilliseconds =
+        DateTime(date.end.day).millisecond.toString();
 
     String urL = 'API_HERE';
     var url = Uri.parse(
       urL,
     );
     Map<String, String> dateFilter = {
-      'startDate': startUtc.toIso8601String(),
-      'endDate': endUtc.toIso8601String(),
-      'startTime': startMs.toString(),
-      'endTime': endMs.toString()
+      'startDate': startDateTimeUtc,
+      'endDate': endDateTimeUtc,
+      'startTime': startDateTimeMilliseconds,
+      'endTime': endDateTimeMilliseconds,
     };
     try {
-      final response = await http.get(url, headers: dateFilter);
+      final response = await http.post(url, body: json.encode(dateFilter));
 
       print(response.statusCode);
+
       if (response.statusCode == 200) {
         final extractedData = json.decode(response.body);
+
         if (extractedData == null) {
           Get.snackbar('Error', 'Could not load data');
           return;
         } else if (response.statusCode > 400) {
           Get.snackbar('error', 'some error occurred');
         }
+        // json.decode extracted data to result(map declared) and pass the data to the graphs and rebuild theme accordingly.
       }
     } catch (error) {
       print(error);
       throw (error);
     }
-    // put extracted data to result(map declared) and pass the data to the graphs and rebuild theme accordingly.
   }
 }
